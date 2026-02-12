28994b7235228c03e47554c006992fddfa5d892b
priyyshe <ppryynt@gmail.com>
2026-02-12 13:32:08 +0700

    Update login-script.js
    mengganti komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,22 +1,56 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+/** 
+ * @module Login-Script
  */
 
+/**
+ * Form login pada halaman
+ * @constant {HTMLElement}
+ */
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+ * Input email dari form login
+ * @constant {HTMLElement}
+ */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+ * Input password dari form login
+ * @constant {HTMLElement}
+ */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+ * Email yang digunakan untuk validasi login
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+ * Password yang digunakan untuk validasi login
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+
+/**
+ * Event submit pada form login
+ */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+   * Nilai email yang dimasukkan pengguna
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+
+  /**
+   * Nilai password yang dimasukkan pengguna
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
+
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
